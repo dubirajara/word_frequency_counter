605b55c0a2404a4d579c2c45e8298bf222ab8fdc
Diego Ubirajara <diego.ubirajara@mail.com>
2020-10-06 18:55:24 +0200

    refactor code

--- words_frequency_counter.py
+++ words_frequency_counter.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from itertools import chain
 from operator import itemgetter
-from typing import Iterator
+from typing import Iterator, Generator
 import re
 import string
 
@@ -9,31 +9,43 @@ import requests
 
 from report import generate_report
 
-urls = ('https://storage.googleapis.com/apache-beam-samples/shakespeare/kinglear.txt',
-        'https://storage.googleapis.com/apache-beam-samples/shakespeare/othello.txt',
-        'https://storage.googleapis.com/apache-beam-samples/shakespeare/romeoandjuliet.txt')
 
-data_url = (requests.get(url).text.lower().split() for url in urls)  # Get urls data and convert in words lists
-data_url = chain.from_iterable(data_url)  # Join three iterable words lists in only one.
+def get_words() -> Iterator:
+    """Get data"""
+    urls = ('https://storage.googleapis.com/apache-beam-samples/shakespeare/kinglear.txt',
+            'https://storage.googleapis.com/apache-beam-samples/shakespeare/othello.txt',
+            'https://storage.googleapis.com/apache-beam-samples/shakespeare/romeoandjuliet.txt')
+
+    data_url = (requests.get(url).text.lower().split() for url in urls)
+    data_url = chain.from_iterable(data_url)
+    return data_url
 
 
-def clean_words(data: Iterator) -> list:
-    """Clean the data, punctuation and irrelevant words"""
-    re_path = re.compile(f'[{re.escape(string.punctuation)}]')  # regex pattern
-    words_clean_p = (re_path.sub('', w) for w in data)  # Clean punctuation
+def clean_words(data: Iterator) -> Generator:
+    """Clean the data"""
+    re_path = re.compile(f'[{re.escape(string.punctuation)}]')
+    return (re_path.sub('', w) for w in data)
 
+
+def get_relevant_words(words: Iterator) -> Generator:
+    """Get relevant words"""
     with open("StopWords.txt", "r") as file:
-        stop_words = [word.strip() for word in file]  # Get stop words data to relevant words classification
-    relevant_words = [x for x in words_clean_p if x not in stop_words]  # Get only relevant words
-
-    return relevant_words
+        stop_words = [word.strip() for word in file]
+    return (x for x in words if x not in stop_words)
 
 
-def word_frequency_counter(words: list) -> dict:
+def word_frequency_counter(relevant_words: Generator) -> dict:
     """Count and analyze word frequencies"""
-    count_word_freq = Counter(words)
-    return dict(sorted(count_word_freq.items(), key=itemgetter(1), reverse=True))  # Dict ordered by frequency (gtl)
+    count_word_freq = Counter(relevant_words)
+    return dict(sorted(count_word_freq.items(), key=itemgetter(1), reverse=True))
+
+
+def main():
+    data_url = get_words()
+    words = clean_words(data_url)
+    relevant_words = get_relevant_words(words)
+    generate_report(word_frequency_counter(relevant_words))
 
 
 if __name__ == '__main__':
-    generate_report(word_frequency_counter(clean_words(data_url)))
+    main()
